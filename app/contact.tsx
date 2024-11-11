81c15f0b945b70286d6c3c9961e424b82edccd28
marcasro9 <marcasro9@gmail.com>
2024-11-11 00:30:46 -0600

    Fix search contact

--- app/contact.tsx
+++ app/contact.tsx
@@ -83,7 +83,7 @@ export default function Contact() {
     const filteredContacts = sortedContacts.filter((contact: any) => {
       const fullName =
         `${contact.name || ""} ${contact.lastName || ""}`.toLowerCase();
-      const phoneNumber = `${contact.phoneNumbers[0].number}`;
+      const phoneNumber = `${contact.phoneNumbers[0].number.substring(5)}`;
       const searchTerm = search.toLowerCase();
 
       return fullName.includes(searchTerm) || phoneNumber.includes(searchTerm);
